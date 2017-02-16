28782c383bd7b79e099d4f6d3bd55f05c1cd43c9
Claire Leslie <c.leslie@live.ca>
2017-02-16 16:26:59 -0500

    add buttons for each step and create ajax requests for each

--- ajax.js
+++ ajax.js
@@ -13,8 +13,42 @@ $(document).ready(function () {
       method: 'GET',
       dataType: 'text'
       }).done(function (responseData){
-        console.log(responseData)
+        console.log("Sweet success!")
         $('#step3456').append(responseData)
+      }).fail(function (){
+        console.log("Request failed :(")
+        $('#step3456').append("your page did not load, I will try harder next time!!")
+      }).always(function() {
+        console.log("Request complete")
+      });
+    });
+    $('.count').on('click', function(){
+      $.ajax ({
+      url: 'http://first-ajax-api.herokuapp.com/count',
+      method: 'GET',
+      dataType: 'text'
+    }).done(function (responseData){
+      $('#step7').append(responseData)
     });
   });
+    $('.time').on('click', function(){
+      $.ajax ({
+        url: 'http://first-ajax-api.herokuapp.com/time',
+        method: 'GET',
+        data: {timezone: 'Pacific/Honolulu'},
+        dataType: 'text'
+      }).done(function (responseData){
+        $('#step8').append(responseData)
+      });
+    });
+    $('.car').on('click', function(){
+      $.ajax ({
+      url: 'http://first-ajax-api.herokuapp.com/a_car',
+      method: 'GET',
+      dataType: 'html',
+    }).done(function (responseData){
+      $('#car-list').append(responseData)
+    });
+    });
+
 });
